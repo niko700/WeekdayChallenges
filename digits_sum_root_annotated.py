b98c15050c5533b64e103bcaa981621f92f72e04
Vera Neroni <vnneroni@gmail.com>
2023-01-12 00:00:29 -0500

    code with test

---
+++ digits_sum_root_annotated.py
@@ -0,0 +1,45 @@
+#this fails all the tests
+
+from __future__ import annotations
+from beginnercodes.challenges import test
+
+def root_digit(number: int) -> int:
+    def make_text_list(k):
+
+        a = str(k)          #converts number entered as parameter to text
+
+        i = 0
+        l = []
+
+        for i in a:             #splits digits in input number into text digit list
+            l.append(i)
+
+        return l
+
+    def make_num_list(m):
+
+        a = str(m)          #converts number entered as parameter to text
+
+        i = 0
+        l = []
+        l_num = []
+
+        for i in a:             #splits digits in input number into text digit list
+            l.append(i)
+
+        for i in l:             #converts digits in list into numbers
+            digit_as_num = l_num.append(int(i))
+
+        return l_num
+
+    def r_d(n):
+
+        s = sum(make_num_list(n))
+        length = len(make_num_list(n))
+
+        while length > 1:
+            return r_d(s)
+
+        return s
+
+test(587, root_digit)
