e818dfb4ea7da94e5eaf7d8855ce4381fbbdc43b
Vera Neroni <vnneroni@gmail.com>
2023-01-11 23:18:03 -0500

    first commit

---
+++ digits_sum_root.py
@@ -0,0 +1,37 @@
+def make_text_list(k):
+
+   a = str(k)          #converts number entered as parameter to text
+
+   i = 0
+   l = []
+
+   for i in a:             #splits digits in input number into text digit list
+        l.append(i)
+
+   return l
+
+def make_num_list(m):
+
+    a = str(m)          #converts number entered as parameter to text
+
+    i = 0
+    l = []
+    l_num = []
+
+    for i in a:             #splits digits in input number into text digit list
+        l.append(i)
+
+    for i in l:             #converts digits in list into numbers
+        digit_as_num = l_num.append(int(i))
+
+    return l_num
+
+def root_digit(n):
+
+    s = sum(make_num_list(n))
+    length = len(make_num_list(n))
+
+    while length > 1:
+        return r_d(s)
+
+    return s
